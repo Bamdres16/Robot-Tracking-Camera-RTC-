b9c1c89f969759e708d866907b8bd998b408b31f
Bamdres16 <brianm.bra@estudiantec.cr>
2021-04-27 11:27:35 -0600

    Ignore files

--- test.py
+++ test.py
@@ -2,6 +2,16 @@ import cv2 as cv
 import threading as th
 import sys
 
+def checkCameras ():
+	index = 0
+	arr = []
+	for i in range(10):
+		cap = cv.VideoCapture(i)
+		if cap.read()[0]:
+			arr.append(i)
+			cap.release()
+	return arr
+
 class camThread (th.Thread):
 	def __init__(self, previewName, camID):
 		th.Thread.__init__(self)
@@ -12,7 +22,7 @@ class camThread (th.Thread):
 		camPreview(self.previewName, self.camID)
 		
 def camPreview(previewName, camID):
-	cv.namedWindow(previewName)
+	#cv.namedWindow(previewName)
 	cam = cv.VideoCapture(camID)
 	fourcc = cv.VideoWriter_fourcc(*'XVID')
 	out = cv.VideoWriter('out_' + previewName + '.avi', fourcc, 10.0, (640,480))
@@ -26,15 +36,20 @@ def camPreview(previewName, camID):
 		#cv.imshow(previewName, frame)
 		out.write(frame)
 		rval, frame = cam.read()
-		if cv.waitKey(1) == ord('q'):
+		if cv.waitKey(20) == ord('q'):
 			break
 	cv.destroyWindow(previewName)
 	cam.release()
 
-camID = int(sys.argv[1])
-thread1 = camThread('Source'+str(camID), camID)
+#camID = int(sys.argv[1])
 
-thread1.start()
+camerasAvailable = [0,2,4]
+
+for camID in camerasAvailable:
+	thread1 = camThread('Source'+str(camID), camID)
+	thread1.start()
+
+
 
 
 		
