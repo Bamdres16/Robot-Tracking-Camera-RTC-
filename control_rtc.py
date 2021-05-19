d3ca11675c487906c090ba912e6a62035d78c0dc
Bamdres16 <brianm.bra@estudiantec.cr>
2021-05-19 16:18:28 -0600

    Se modifican los nombres de los componentes para mejor lectura

--- control_rtc.py
+++ control_rtc.py
@@ -1,6 +1,6 @@
 from rtc_ui import *
 import time
-from grabacion.cameraRecord import *
+#from grabacion.cameraRecord import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPixmap
 import os
@@ -9,27 +9,64 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
             QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
             self.setupUi(self)
-            self.camerasAvailable = checkCameras()
-            self.camerasUsed = []
-            # Añadir función a botón
+            self.camerasAvailable = {"0": True, "2": True, "3":True}
             #self.pushButton.clicked.connect(self.changeProgress)
-            self.comboBox.addItems(self.camerasAvailable)
-            self.comboBox.currentIndexChanged.connect(self.selectCamera)
-            self.btnSrc1.clicked.connect(self.checkPath)
+            
+            self.updateCombos()
+            
+            # Buttons events
+            self.btnSrc1.clicked.connect(self.selectSource1)
+            self.btnSrc2.clicked.connect(self.selectSource2)
+            self.btnSrc3.clicked.connect(self.selectSource3)
+            self.btnSrc4.clicked.connect(self.selectSourceExtra)
 
-    def selectCamera(self, i):
+
+    def selectSource1(self, i):
+        #self.graphicsView
+        #takePreviewImage(int(self.camerasAvailable[i]), "img_0.jpg")
+        print("Ok 1")
+    
+    def selectSource2(self, i):
+        #self.graphicsView_3
+        #takePreviewImage(int(self.camerasAvailable[i]), "img_0.jpg")
+        print("Ok 2")
         
-        takePreviewImage(int(self.camerasAvailable[i]), "img_0.jpg")
+    def selectSource3(self, i):
+        #self.graphicsView_2
+        #takePreviewImage(int(self.camerasAvailable[i]), "img_0.jpg")
+        print("Ok 3")
         
+    def selectSourceExtra(self, i):
+        #self.graphicsView_4
+        #takePreviewImage(int(self.camerasAvailable[i]), "img_0.jpg")
+        print("Ok 4")
         
-    def checkPath(self):
+    def updateCombos(self):
+        cameraKeys = self.getKeysAvailable()
+        self.comboSrc1.addItems(cameraKeys)
+        self.comboSrc2.addItems(cameraKeys)
+        self.comboSrc3.addItems(cameraKeys)
+        self.comboSrc4.addItems(cameraKeys)
+    
+    def getKeysAvailable(self):
+        cameraKeys = []
+        for i in self.camerasAvailable.keys():
+            if self.camerasAvailable[i] == True:
+                cameraKeys.append(i)
+        return cameraKeys
+    
+    def toggleStateCamera(self, key):
+        current = self.camerasAvailable[key]
+        self.camerasAvailable[key] = not current
+        
+    ''' def checkPath(self):
         image_path = "img_0.jpg"
         if os.path.isfile(image_path):
             scene = QtWidgets.QGraphicsScene(self)
             pixmap = QPixmap(image_path)
             item = QtWidgets.QGraphicsPixmapItem(pixmap)
             scene.addItem(item)
-            self.graphicsView.setScene(scene)
+            self.graphicsView.setScene(scene) '''
         
       
 if __name__ == "__main__":
