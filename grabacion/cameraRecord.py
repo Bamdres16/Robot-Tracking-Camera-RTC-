f01e25139d5533e9d3d5f76e63e9fed77fc8950c
Bamdres16 <brianm.bra@estudiantec.cr>
2021-06-01 16:03:17 -0600

    Videos generated in folder, and final video format.

--- grabacion/cameraRecord.py
+++ grabacion/cameraRecord.py
@@ -1,19 +1,21 @@
-## Este codigo fue desarrollado por Brayan Munoz Mora cualquier consulta
-## comunicarse al correo brayanmunozmora16@gmail.com.
-## Aca se anaden las funciones principales para capturar los videos, tomar snapshots
-## asi como unir fuentes de videos de un folder en especifico.
+# Este codigo fue desarrollado por Brayan Munoz Mora cualquier consulta
+# comunicarse al correo brayanmunozmora16@gmail.com.
+# Aca se anaden las funciones principales para capturar los videos, tomar snapshots
+# asi como unir fuentes de videos de un folder en especifico.
 
 import cv2 as cv
 import os
 import time
+from datetime import datetime
 
-## Variables globales
-width = 640
-height = 480
+# Variables globales
+rootPathVideos = "Videos/"
 
-## Mediante esta funcion se pretende obtener todas aquellas fuentes de video
-## que se pueden abrir de los recursos de video (camaras) disponibles en el sistema.
-def checkCameras ():
+# Mediante esta funcion se pretende obtener todas aquellas fuentes de video
+# que se pueden abrir de los recursos de video (camaras) disponibles en el sistema.
+
+
+def checkCameras():
 	index = 0
 	arr = {}
 	for i in range(10):
@@ -25,28 +27,35 @@ def checkCameras ():
 	print(arr)
 	return arr
 
-## Aca podemos grabar un video desde un fuente (camID), el video se va a almacenar en la ruta
-## que se especifique en videoName, y la duracion de grabacion se pasa en el parametro 
-## duration (el cual esta en segundos).
-def recordVideo(videoName, camID, duration, width = 640, height = 480, fps = 10.0):
-	source = cv.VideoCapture(camID)
+# Aca podemos grabar un video desde un fuente (camID), el video se va a almacenar en la ruta
+# que se especifique en videoName, y la duracion de grabacion se pasa en el parametro
+# duration (el cual esta en segundos).
 
-	fourcc = cv.VideoWriter_fourcc(*'XVID')
-	out = cv.VideoWriter(videoName + '.avi', fourcc, fps, (width,height))
-	
-	if source.isOpened():
-		_, frame = source.read()
-	current = time.time()
-	while (time.time() - current) < duration:
-		out.write(frame)
-		_, frame = source.read()
-  
-		if cv.waitKey(1) == ord('q'):
-			break
-	source.release()
 
-## Esta funcion toma una imagen de la fuente especificada (camID), y se almacena en la ruta provista en el
-## parametro imageName.
+def recordVideo(videoName, camID, duration, width=640, height=480, fps=10.0):
+    now = datetime.now()
+    today = str(now.strftime("%d-%m-%Y"))
+    filePath = rootPathVideos + videoName + "/" + today
+    source = cv.VideoCapture(camID)
+    fourcc = cv.VideoWriter_fourcc(*'XVID')
+    if not os.path.exists(filePath):
+        os.makedirs(filePath)
+    
+    videoIndex = len([n for n in os.listdir(filePath) if n[0] != 'f'])
+    out = cv.VideoWriter(filePath + "/" + str(videoIndex) + "_" + today + '.avi', fourcc, fps, (width, height))
+    if source.isOpened():
+        _, frame = source.read()
+    current = time.time()
+    while (time.time() - current) < duration:
+        out.write(frame)
+        _, frame = source.read()
+        if cv.waitKey(1) == ord('q'):
+            break
+    source.release()
+    print("Video created in", filePath)
+
+# Esta funcion toma una imagen de la fuente especificada (camID), y se almacena en la ruta provista en el
+# parametro imageName.
 def takePicture (camID, imageName):
 	source = cv.VideoCapture(camID)
 	_,frame = source.read()
@@ -54,18 +63,23 @@ def takePicture (camID, imageName):
 	if _ and frame is not None:
 		cv.imwrite(imageName, frame)
   
-## Con esta funcion se genera la mezcla del video final, aca los videos estan almacenados con un un formato id_formato.avi,
-## el id corresponde al orden en el que fue grabado, es por esta razon que la mezcla se realiza en base a ese orden.
-## En este caso rootSources es la ruta del folder en donde se encuentran todos los videos, y dest es la ruta en donde se guardara
-## el video mezclado final, en este caso unicamente se pasa el nombre de como se quiere guardar, ya que el video se guarda en la misma
-## ruta que los videos separados.
-def generateVideo (rootSources, dest, width = 640, height = 480, fps = 10.0):
-    videoFiles = [n for n in os.listdir(rootSources) if n[-4:] == '.avi']
+# Con esta funcion se genera la mezcla del video final, aca los videos estan almacenados con un un formato id_formato.avi,
+# el id corresponde al orden en el que fue grabado, es por esta razon que la mezcla se realiza en base a ese orden.
+# En este caso rootSources es la ruta del folder en donde se encuentran todos los videos, y dest es la ruta en donde se guardara
+# el video mezclado final, en este caso unicamente se pasa el nombre de como se quiere guardar, ya que el video se guarda en la misma
+# ruta que los videos separados.
+def generateVideo (rootSources, width = 640, height = 480, fps = 10.0):
+    now = datetime.now()
+    today = str(now.strftime("%d-%m-%Y"))
+    filePath = rootPathVideos + rootSources
+    videoFiles = [n for n in os.listdir(filePath) if (n[-4:] == '.avi') and (n[0] != "f")]
+    print(videoFiles)
+   
     index = 0
     
-    source = cv.VideoCapture(rootSources + videoFiles[0])
+    source = cv.VideoCapture(filePath + "/" + videoFiles[0])
     fourcc = cv.VideoWriter_fourcc(*'XVID')
-    out = cv.VideoWriter(rootSources + dest + '.avi', fourcc, fps, (width,height))
+    out = cv.VideoWriter(filePath + "/" + "f_" + today + '.avi', fourcc, fps, (width,height))
     
     # Empezamos el proceso para unir los videos
     while (source.isOpened()):
@@ -74,16 +88,17 @@ def generateVideo (rootSources, dest, width = 640, height = 480, fps = 10.0):
             index += 1
             if index >= len(videoFiles):
                 break
-            source = cv.VideoCapture(rootSources + videoFiles[index])
+            source = cv.VideoCapture(filePath + "/" + videoFiles[index])
             ret, frame = source.read()
         out.write(frame)
     source.release()
     out.release()
     print("End")
     
-## Codigo de ejemplo
-recordVideo("BrayanMunozMora_Prof_LiceoCoronado", 0, 5)
-recordVideo("BrayanMunozMora_Prof_LiceoCoronado", 2, 5)
-recordVideo("BrayanMunozMora_Prof_LiceoCoronado", 4, 5)
-#generateVideo("Videos\Estudiante_Prof_Institucion\Corrida2/", "videoExample")
+# Codigo de ejemplo
+''' recordVideo("BrayanMunozMora_Prof_LiceoCoronado", 0, 1)
+recordVideo("BrayanMunozMora_Prof_LiceoCoronado", 0, 1)
+recordVideo("BrayanMunozMora_Prof_LiceoCoronado", 0, 1)
+recordVideo("BrayanMunozMora_Prof_LiceoCoronado", 0, 1)
+generateVideo("BrayanMunozMora_Prof_LiceoCoronado/01-06-2021") '''
 		
